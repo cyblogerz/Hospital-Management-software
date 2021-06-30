13f1ded2464ad24883eac5080b8f0004d309dbd7
warmachine028 <warmachine028@outlook.com>
2021-06-30 19:14:56 +0530

    Refactoring database.py

--- Main/database.py
+++ Main/database.py
@@ -1,149 +1,110 @@
-import sqlite3
+from sqlite3 import connect
+from sqlite3.dbapi2 import Connection, Cursor
 
 
+class Medicine:
+    def __init__(self, company: str, expiry: str, name: str, price: int) -> None:
+        self.company = company
+        self.expiry = expiry
+        self.name = name
+        self.price = price
 
-def add_store(med, stock):
-    conn = sqlite3.connect('HMS.db')
-    c = conn.cursor()
-    c.execute("INSERT INTO store VALUES(?,?,?,?,?)",
-              (med.name, med.company, med.expiry, stock, med.price))
-    conn.commit()
-    conn.close()
-
-
-def add_pharma(med, stock):
-    conn = sqlite3.connect('HMS.db')
-    c = conn.cursor()
-    c.execute("INSERT INTO store VALUES(?,?,?,?,?)",
-              (med.name, med.company, med.expiry, stock, med.price))
-    conn.commit()
-    conn.close()
+    def __str__(self) -> str:
+        return f'Name:{self.name}\nCompany:{self.company}\nExpiry:{self.expiry}'
 
 
-def rem_store(med, st):
-    conn = sqlite3.connect('HMS.db')
-    c = conn.cursor()
-    c.execute("SELECT rowid,* FROM store WHERE name=(?)", med.name)
-    m = c.fetchall()
+class Patient:
+    def __init__(self, name: str, place: str, phone: str, age: int, visit: int) -> None:
+        self.name = name
+        self.place = place
+        self.age = age
+        self.phone = phone
+        self.visit = visit
+
+
+def connect_cursor() -> tuple[Connection, Cursor]:
+    con: Connection = connect("HMS.db")
+    cur: Cursor = con.cursor()
+    return con, cur
+
+
+def commit_close(con: Connection) -> None:
+    con.commit()
+    con.close()
+
+
+def addvalue(med: Medicine, stock: int) -> None:
+    con, cur = connect_cursor()
+    value = med.name, med.company, med.expiry, stock, med.price
+    cur.execute(f"INSERT INTO store VALUES{value}")
+    commit_close(con)
+
+
+def issue(med: Medicine, st: int, place: str) -> None:
+    con, cur = connect_cursor()
+    cur.execute(f"SELECT rowid,* FROM {place} WHERE name = {med.name}")
     stock = 0
-    for i in m:
+    for i in cur.fetchall():
         stock += i[3]
     if stock == st:
-        c.execute("DELETE FROM store WHERE name=(?)", med.name)
+        con.execute(f"DELETE FROM {place} WHERE name = {med.name}")
     elif stock < st:
-        print('Not enough stock')
+        print("Not enough stock")
     else:
-        c.execute("UPDATE store SET stock=stock-st")
-
-    conn.commit()
-    conn.close()
+        con.execute(f"UPDATE {place} SET stock = {stock - st}")
+    commit_close(con)
 
 
-def pharma_issue(med, st):
-    conn = sqlite3.connect('HMS.db')
-    c = conn.cursor()
-    c.execute("SELECT rowid,* FROM pharmacy WHERE name=(?)", med.name)
-    m = c.fetchall()
-    stock = 0
+def add_paient(pat: Patient) -> None:
+    con, cur = connect_cursor()
+    values = pat.name, pat.age, pat.place, pat.phone, pat.visit
+    cur.execute(f"INSERT INTO patients VALUES{values}")
+    commit_close(con)
+
+
+def view(place: str) -> str:
+    con, cur = connect_cursor()
+    cur.execute(f"SELECT * FROM {place}")
+    s = "Name:\tCompany:\tExpiryDate:\tStock:\tPrice:\n"
+    for i in cur.fetchall():
+        s += f"{i[0]}\t{i[1]}\t{i[2]}\t{i[3]}\t{i[4]}\n"
+    commit_close(con)
+    return s
+
+
+def patient_list() -> str:
+    con, cur = connect_cursor()
+    cur.execute("SELECT rowid,* FROM patients")
+    s = "Regno:\tName:\tAge:\tPlace:\tPhone:\tVisit:\n"
+    for i in cur.fetchall():
+        s += f"{i[0]}\t{i[1]}\t{i[2]}\t{i[3]}\t{i[4]}\t{i[5]}\n"
+    commit_close(con)
+    return s
+
+
+def reg_no(regno: str) -> str:
+    con, cur = connect_cursor()
+    cur.execute(f"UPDATE patients SET visit=visit+1 WHERE rowid = {regno}")
+    cur.execute(f"SELECT rowid,* FROM patients WHERE rowid = {regno}")
+    s = "Regno:\tName:\tAge:\tPlace:\tPhone:\tVisit:\n"
+    for i in cur.fetchall():
+        s += f"{i[0]}\t{i[1]}\t{i[2]}\t{i[3]}\t{i[4]}\t{i[5]}\n"
+    commit_close(con)
+    return s
+
+
+def med_search(med_name: str) -> Medicine:
+    con, cur = connect_cursor()
+    cur.execute(f"SELECT * FROM store WHERE name = {med_name}")
+    m = cur.fetchall()
+    name = ""
+    company = ""
+    expiry_date = ""
+    price = 0
     for i in m:
-        stock += i[3]
-
-    if stock == st:
-
-        c.execute("DELETE FROM pharmacy WHERE name=(?)", med.name)
-    elif stock < st:
-        print('stock is not enough')
-
-    else:
-        c.execute("UPDATE pharmacy SET stock=stock-st")
-
-    conn.commit()
-    conn.close()
-
-
-def add_pt(pat):
-    conn = sqlite3.connect('HMS.db')
-
-    c = conn.cursor()
-    c.execute("INSERT INTO patients VALUES(?,?,?,?,?)",
-              (pat.name, pat.age, pat.place, pat.phone, pat.visit))
-    conn.commit()
-    conn.close()
-
-
-#
-def view_store():
-    conn = sqlite3.connect('HMS.db')
-    c = conn.cursor()
-    c.execute("SELECT * FROM store")
-    st = c.fetchall()
-    conn.commit()
-    conn.close()
-    s = 'Name:\tCompany:\tExpiryDate:\tStock:\tPrice:\n'
-    for i in st:
-        s += f'{i[0]}\t{i[1]}\t{i[2]}\t{i[3]}\t{i[4]}\n'
-    return s
-
-
-def view_pharma():
-    conn = sqlite3.connect('HMS.db')
-    c = conn.cursor()
-    c.execute("SELECT * FROM pharmacy")
-    st = c.fetchall()
-    conn.commit()
-    conn.close()
-    s = 'Name:\tCompany:\tExpiryDate:\tStock:\tPrice:\n'
-    for i in st:
-        s += f'{i[0]}\t{i[1]}\t{i[2]}\t{i[3]}\t{i[4]}\n'
-    return s
-
-
-def reg_no(regno):
-    conn = sqlite3.connect('HMS.db')
-    c = conn.cursor()
-    c.execute("UPDATE patients SET visit=visit+1 WHERE rowid=(?)", regno)
-    c.execute("SELECT rowid,* FROM patients WHERE rowid= (?)", regno)
-    pat = c.fetchall()
-    conn.commit()
-    conn.close()
-    s = 'Regno:\tName:\tAge:\tPlace:\tPhone:\tVisit:\n'
-    for i in pat:
-        s += f'{i[0]}\t{i[1]}\t{i[2]}\t{i[3]}\t{i[4]}\t{i[5]}\n'
-    return s
-
-
-def pat_list():
-    conn = sqlite3.connect('HMS.db')
-    c = conn.cursor()
-
-    c.execute("SELECT rowid,* FROM patients")
-    p = c.fetchall()
-    conn.commit()
-    conn.close()
-    s = 'Regno:\tName:\tAge:\tPlace:\tPhone:\tVisit:\n'
-    for i in p:
-        s += f'{i[0]}\t{i[1]}\t{i[2]}\t{i[3]}\t{i[4]}\t{i[5]}\n'
-    return s
-
-
-def med_search(name):
-    conn = sqlite3.connect('HMS.db')
-    c = conn.cursor()
-
-    c.execute("SELECT * FROM store WHERE name = (?)", name)
-
-    m = c.fetchall()
-
-    conn.commit()
-    conn.close()
-    Name = ''
-    Company = ''
-    ExpiryDate = ''
-    Price = 0
-    for i in m:
-        Name += i[0]
-        Company += i[1]
-        ExpiryDate += i[2]
-        Price += i[4]
-    med = Medicine(Company, ExpiryDate, Name, Price)
-    return med
+        name += i[0]
+        company += i[1]
+        expiry_date += i[2]
+        price += i[4]
+    commit_close(con)
+    return Medicine(company, expiry_date, name, price)
